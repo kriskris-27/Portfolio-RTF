24fbfd9c10e255a7de71461c68a9e273f47ff053
Kris <krisnakumars0101@gmail.com>
2025-03-13 09:17:04 +0530

    contact and footer bug fixed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,30 +3,11 @@ import { motion } from "framer-motion";
 
 const Contact = () => {
   return (
-    <section id="contact" className="py-40 text-white text-center relative ">
-    <div className="z-100 bg-gradient-to-br from-black via-[#1a1a2e] to-[#16213e]
+    <section id="contact" className="py-40 text-white text-center relative bg-gradient-to-br from-black via-[#1a1a2e] to-[#16213e]">
+    <div className="z-100 
     p-5
 ">
-      <motion.div
-        className="absolute top-6 left-6 text-xl font-bold text-gray-300"
-        initial={{ opacity: 0, x: -50, rotateY: 90 }}
-        animate={{ opacity: 1, x: 0, rotateY: 0 }}
-        transition={{ duration: 1, ease: "easeOut", delay: 0.3 }}
-      >
-        <motion.span
-  initial={{ opacity: 0, scale: 0.8 }}
-  animate={{ opacity: 1, scale: 1.2 }}
-  transition={{
-    duration: 2, // Slower animation
-    repeat: Infinity,
-    repeatType: "reverse",
-    ease: "easeInOut",
-  }}
-  className="text-transparent bg-clip-text bg-gradient-to-b from-[#FAF6F2] via-[#E8D7F1] to-[#C2E9FB]"
->
-  Krishnakumar S
-</motion.span>
-      </motion.div>
+     
 
       {/* Title Animation */}
       <motion.h2
@@ -68,7 +49,29 @@ const Contact = () => {
           LinkedIn
         </a>
       </motion.div>
+
+      <motion.div
+        className="absolute bottom-15 left-6 text-xl font-bold text-gray-300"
+        initial={{ opacity: 0, x: -50, rotateY: 90 }}
+        animate={{ opacity: 1, x: 0, rotateY: 0 }}
+        transition={{ duration: 1, ease: "easeOut", delay: 0.3 }}
+      >
+        <motion.span
+  initial={{ opacity: 0, scale: 0.8 }}
+  animate={{ opacity: 1, scale: 1.2 }}
+  transition={{
+    duration: 2, // Slower animation
+    repeat: Infinity,
+    repeatType: "reverse",
+    ease: "easeInOut",
+  }}
+  className="text-transparent bg-clip-text bg-gradient-to-b from-[#FAF6F2] via-[#E8D7F1] to-[#C2E9FB]"
+>
+  Krishnakumar S
+</motion.span>
+      </motion.div>
       </div>
+
     </section>
   );
 };
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,6 @@
 const Footer = () => {
     return (
-      <footer className="p-0 relative bg-black
-before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-gradient-radial before:from-[#111] before:via-[#000] before:to-[#111] before:opacity-80 
+      <footer className="p-0 relative  bg-gradient-to-br from-black via-[#1a1a2e] to-[#16213e]
 text-center text-white">
         <p>&copy; 2025 Imkris. All rights reserved.</p>
       </footer>
