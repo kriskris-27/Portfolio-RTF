0cb2a888064d9263a2e796dc15728f8346553de6
Kris <krisnakumars0101@gmail.com>
2025-03-14 11:19:32 +0530

    added text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,14 @@
+import { ScreenFitText } from "@/framer/Screenfit";
+import { VelocityText } from "@/framer/Velocitytext";
+
 const Footer = () => {
     return (
       <footer className="p-0 relative  bg-gradient-to-br from-black via-[#1a1a2e] to-[#16213e]
 text-center text-white">
+
+    <ScreenFitText/>
         <p>&copy; 2025 Imkris. All rights reserved.</p>
+    {/* <VelocityText/> */}
       </footer>
     );
   };
